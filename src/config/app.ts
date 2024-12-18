4b12360122f429f1fc65c6647d4d4abcb43beddb
Dave Njoroge <davenjorge10@gmail.com>
2024-12-18 19:08:42 +0300

    update api key

--- src/config/app.ts
+++ src/config/app.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  port: process.env["PORT"] ?? "8000",
+  port: process.env["API_PORT"] ?? "8000",
   app_name: process.env["APP_NAME"],
 };
 
